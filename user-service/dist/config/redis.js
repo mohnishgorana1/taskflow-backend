7661b3c6d8ce25f41b7b0cc7968e17a39819bbf6
Mohnish Gorana <mohnishgorana1@gmail.com>
2025-03-07 12:58:02 +0530

    update redis

--- user-service/dist/config/redis.js
+++ user-service/dist/config/redis.js
@@ -1,12 +1,20 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.redisClient = void 0;
 const redis_1 = require("redis");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const redisUrl = process.env.REDIS_URL;
+console.log("redis url", process.env.REDIS_URL);
 const redisClient = (0, redis_1.createClient)({
-    url: process.env.REDIS_URL || "redis://localhost:6379",
+    url: process.env.REDIS_URL,
 });
 exports.redisClient = redisClient;
 redisClient.on("error", (err) => console.error("❌ Redis Client Error:", err));
-redisClient.connect()
+redisClient
+    .connect()
     .then(() => console.log("✅ Connected to Redis"))
     .catch((err) => console.error("❌ Redis Connection Failed:", err));
--- user-service/src/config/redis.ts
+++ user-service/src/config/redis.ts
@@ -1,16 +1,20 @@
 import { createClient } from "redis";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+const redisUrl = process.env.REDIS_URL;
+console.log("redis url", process.env.REDIS_URL);
 
 const redisClient = createClient({
-    url: process.env.REDIS_URL,
-    socket: {
-        reconnectStrategy: (retries) => Math.min(retries * 50, 1000), // Retry strategy
-    },
+  url: process.env.REDIS_URL,
 });
 
 redisClient.on("error", (err) => console.error("❌ Redis Client Error:", err));
 
-redisClient.connect()
-    .then(() => console.log("✅ Connected to Redis"))
-    .catch((err) => console.error("❌ Redis Connection Failed:", err));
+redisClient
+  .connect()
+  .then(() => console.log("✅ Connected to Redis"))
+  .catch((err) => console.error("❌ Redis Connection Failed:", err));
 
 export { redisClient };
